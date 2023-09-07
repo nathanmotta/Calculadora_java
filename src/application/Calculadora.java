5354018f25984bf57ff81310fe092948b7e89250
nathanmotta <142407439+nathanmotta@users.noreply.github.com>
2023-09-07 17:41:34 -0300

    Add files via upload

---
+++ src/application/Calculadora.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Calculadora Simples ");
+        System.out.println("Insira o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Insira o segundo número");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Escolha a operação: ");
+        System.out.println("1. Adição");
+        System.out.println("2. Subtração");
+        System.out.println("3. Multiplicação");
+        System.out.println("4. Divisão");
+        System.out.println("Opção: ");
+        int opcao = scanner.nextInt();
+
+        double resultado = 0;
+
+        switch (opcao) {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    System.out.println("Não é possível dividir por zero.");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("Opção inválida.");
+                return;
+        }
+        System.out.println("Resultado: " + resultado);
+    }
+}
